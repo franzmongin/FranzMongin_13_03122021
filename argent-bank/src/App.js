b109fa479ea7f765e203d29d5e0dea36c199f2b3
franzmongin <33622433+franzmongin@users.noreply.github.com>
2021-12-06 18:42:38 +0100

    integrated user page

--- argent-bank/src/App.js
+++ argent-bank/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import SignIn from "./pages/SignIn/SignIn";
+import UserPage from "./pages/UserPage/UserPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/user" element={<UserPage />} />
         </Routes>
       </div>
     </BrowserRouter>
